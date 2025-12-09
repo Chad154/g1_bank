3a88b9a55f33d9fbc2cb31b546f53d5905679b8e
Chad <chad154154@gmail.com>
2025-12-09 10:21:53 +0100

    Added the Customer Fields

--- models/customer.py
+++ models/customer.py
@@ -8,6 +8,17 @@ class Customer(models.Model):
      _description = 'Customer'
 
      name = fields.Char()
+     firstName = fields.String
+     lastName = fields.String
+     middleInitial = fields.Char
+     Street = fields.String
+     State = fields.String
+     zip = fields.Integer
+     phone = fields.Integer
+     email = fields.String
+     password = fields.String
+     
+     
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
