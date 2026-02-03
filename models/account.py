66c85d65f71499b9f335e617bcd91a942ed7fb1a
Dani <danibusiness39@gmail.com>
2026-02-03 14:09:15 +0100

    modelo individual listo

--- models/account.py
+++ models/account.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class g1_Account(models.Model):
     _name = 'g1.account'
@@ -18,8 +19,44 @@ class g1_Account(models.Model):
     )
     balance = fields.Float(string="Balance Actual", default=0.0)
     credit_line = fields.Float(string="Línea de Crédito")
-    opening_date = fields.Date(string="Fecha de Apertura", default=fields.Date.context_today)
+    opening_date = fields.Date(string="Fecha de Apertura", default=fields.Date.context_today, readonly=True)
     begin_balance = fields.Float(string="Balance Inicial")
+    
+    
+    @api.constrains('begin_balance')
+    def _check_amount(self):
+        for account in self:
+            if account.begin_balance < 0:
+                raise ValidationError("The balance must be greater than zero")
+            
+    @api.constrains('account_type', 'credit_line')
+    def _check_credit_line(self):
+        for account in self:
+            if account.account_type == 'standard' and account.credit_line > 0:
+                raise ValidationError("Standard accounts cannot have a credit line")
+                
+    
+    def write(self, vals):
+        if 'name' in vals:
+            raise ValidationError("You cannot change the name.")
+
+        if 'begin_balance' in vals:
+            raise ValidationError("You cannot change the initial balance.")
+
+        if 'balance' in vals:
+            raise ValidationError("You cannot change the balance.")
+        
+        if 'account_type' in vals:
+            raise ValidationError("You cannot change the account type.")
+        
+        if 'credit_line' in vals:
+            for record in self:
+                if record.account_type == 'standard':
+                    raise ValidationError(
+                        "You cannot change the credit line in a standard account"
+                    )
+
+        return super(g1_Account, self).write(vals)
     
 
     # Relación N:1 con Customer
