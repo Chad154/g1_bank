db1535c4d9dfbfc3589bb8858530e2d77aae5623
Dani <danibusiness39@gmail.com>
2026-02-18 09:43:40 +0100

    MEJORA:añadido campo de search en la vista, filtra por tipo de cuentas

--- models/account.py
+++ models/account.py
@@ -5,11 +5,11 @@ from odoo.exceptions import ValidationError
 class g1_Account(models.Model):
     _name = 'g1.account'
     _description = 'Bank Account'
+    _rec_name = 'id'
 	#FIXME Este campo pasa a tener como etiqueta "Descripción"
 	#FIXME Su propósito no será almacenar el ID porque ya existe el campo id heredado de models.Model
-    name = fields.Char(string="Número de Cuenta", required=True)
     #FIXME Eliminar este campo del modelo y de las vistas
-	description = fields.Text(string="Descripción")
+    description = fields.Text(string="Descripción")
     account_type = fields.Selection(
         selection=[
             ('standard', 'Estándar'),
@@ -27,7 +27,7 @@ class g1_Account(models.Model):
     movement_ids = fields.One2many('g1.movement', 'account_id', string="Movimientos")
 	#FIXME añade campo relacional con res.users y asocia la cuenta con el usuario que está creando la cuenta cuando
 	#esta última se cree
-	
+    customer_id = fields.Many2one('res.users', string="Cliente", default=lambda self: self.env.user)
 	#FIXME añade validación para no admitir credit_line negativo
 	
     @api.model
@@ -42,26 +42,23 @@ class g1_Account(models.Model):
         for account in self:
             if account.begin_balance < 0:
                 raise ValidationError("The balance must be greater than zero")
-                
+    
+    #El fixme de credit line negativo esta resuelto aqui
     @api.constrains('account_type', 'credit_line')
     def _check_credit_line(self):
         for account in self:
             if account.account_type == 'standard' and account.credit_line > 0:
                 raise ValidationError("Standard accounts cannot have a credit line")
+            if account.account_type == 'credit' and account.credit_line <= 0:
+                raise ValidationError("Credit accounts must have a credit line greater than 0.")
 
     #FIXME Verifica que se ejecutan los controles que tienes 
 	#en este método cuando se intenta modificar una cuenta
+        #METODO modificado, ahora solo verifica los campos necesarios, el de credit line en standard account ya lo hace la vista automaticamente
     def write(self, vals):
-        if 'name' in vals:
-            raise ValidationError("You cannot change the name.")
         if 'begin_balance' in vals:
             raise ValidationError("You cannot change the initial balance.")
         if 'account_type' in vals:
             raise ValidationError("You cannot change the account type.")
-        
-        if 'credit_line' in vals:
-            for record in self:
-                if record.account_type == 'standard':
-                    raise ValidationError("You cannot change the credit line in a standard account")
 
         return super(g1_Account, self).write(vals)
